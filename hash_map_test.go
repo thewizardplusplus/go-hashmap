606887a639ba678af5822e0a1d3f1ce9e0d7fa7b
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-04-30 02:28:06 +0300

    Add a base of the TestHashMap_Get() test

--- hash_map_test.go
+++ hash_map_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,4 +13,38 @@ func TestNewHashMap(test *testing.T) {
 	require.NotNil(test, hashMap)
 	assert.Len(test, hashMap.buckets, initialCapacity)
 	assert.Zero(test, hashMap.size)
+}
+
+func TestHashMap_Get(test *testing.T) {
+	type fields struct {
+		makeBuckets func() []*bucket
+	}
+	type args struct {
+		makeKey func() Key
+	}
+
+	for _, data := range []struct {
+		name      string
+		fields    fields
+		args      args
+		wantValue interface{}
+		wantOk    assert.BoolAssertionFunc
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			buckets := data.fields.makeBuckets()
+			key := data.args.makeKey()
+			gotValue, gotOk := HashMap{buckets: buckets}.Get(key)
+
+			for _, bucket := range buckets {
+				if bucket != nil {
+					mock.AssertExpectationsForObjects(test, bucket.key)
+				}
+			}
+			mock.AssertExpectationsForObjects(test, key)
+			assert.Equal(test, gotValue, data.wantValue)
+			data.wantOk(test, gotOk)
+		})
+	}
 }
