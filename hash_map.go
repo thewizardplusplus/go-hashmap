a883500dbb5bd772d60fdcdfcef69d59f77d9f59
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-04-30 01:24:26 +0300

    Add the NewHashMap() function

--- hash_map.go
+++ hash_map.go
@@ -16,3 +16,13 @@ type HashMap struct {
 	buckets []*bucket
 	size    int
 }
+
+const (
+	initialCapacity = 16
+)
+
+// NewHashMap ...
+func NewHashMap() *HashMap {
+	buckets := make([]*bucket, initialCapacity)
+	return &HashMap{buckets: buckets, size: 0}
+}
