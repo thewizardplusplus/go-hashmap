fc39afa184685dd16463167553533f47a8edbf55
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-04-30 05:14:17 +0300

    Add the ExampleHashMap() example

---
+++ example_test.go
@@ -0,0 +1,32 @@
+package hashmap
+
+import (
+	"fmt"
+	"hash/fnv"
+	"io"
+)
+
+type StringKey string
+
+func (key StringKey) Hash() int {
+	hash := fnv.New32()
+	io.WriteString(hash, string(key))
+
+	return int(hash.Sum32())
+}
+
+func (key StringKey) Equals(other interface{}) bool {
+	return key == other.(StringKey)
+}
+
+func ExampleHashMap() {
+	timeZones := NewHashMap()
+	timeZones.Set(StringKey("EST"), -5*60*60)
+	timeZones.Set(StringKey("CST"), -6*60*60)
+	timeZones.Set(StringKey("MST"), -7*60*60)
+
+	estOffset, ok := timeZones.Get(StringKey("EST"))
+	fmt.Println(estOffset, ok)
+	// Output:
+	// -18000 true
+}
