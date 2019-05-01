7b33aeea54def8e0ef99ba02290d728645268bd5
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-05-01 13:04:54 +0300

    Implement of the BenchmarkHashMap() benchmark

--- hash_map_bench_test.go
+++ hash_map_bench_test.go
@@ -1,9 +1,25 @@
 package hashmap
 
 import (
+	"encoding/binary"
 	"fmt"
+	"hash/fnv"
+	"math/rand"
 	"testing"
 )
+
+type IntKey int
+
+func (key IntKey) Hash() int {
+	hash := fnv.New32()
+	binary.Write(hash, binary.LittleEndian, int32(key))
+
+	return int(hash.Sum32())
+}
+
+func (key IntKey) Equals(other interface{}) bool {
+	return key == other.(IntKey)
+}
 
 func BenchmarkHashMap(benchmark *testing.B) {
 	for _, data := range []struct {
@@ -11,7 +27,45 @@ func BenchmarkHashMap(benchmark *testing.B) {
 		prepare   func(size int) *HashMap
 		benchmark func(size int, hashMap *HashMap)
 	}{
-		// TODO: add benchmark cases
+		{
+			name: "Get",
+			prepare: func(size int) *HashMap {
+				hashMap := NewHashMap()
+				for i := 0; i < size; i++ {
+					hashMap.Set(IntKey(i), i)
+				}
+
+				return hashMap
+			},
+			benchmark: func(size int, hashMap *HashMap) {
+				hashMap.Get(IntKey(rand.Intn(size)))
+			},
+		},
+		{
+			name:    "Set",
+			prepare: func(size int) *HashMap { return NewHashMap() },
+			benchmark: func(size int, hashMap *HashMap) {
+				for i := 0; i < size; i++ {
+					hashMap.Set(IntKey(i), i)
+				}
+			},
+		},
+		{
+			name: "Delete",
+			prepare: func(size int) *HashMap {
+				hashMap := NewHashMap()
+				for i := 0; i < size; i++ {
+					hashMap.Set(IntKey(i), i)
+				}
+
+				return hashMap
+			},
+			benchmark: func(size int, hashMap *HashMap) {
+				for i := 0; i < size; i++ {
+					hashMap.Delete(IntKey(i))
+				}
+			},
+		},
 	} {
 		for size := 10; size <= 1e6; size *= 10 {
 			name := fmt.Sprintf("%s/%d", data.name, size)
