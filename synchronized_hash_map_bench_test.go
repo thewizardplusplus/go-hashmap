54d410f084573e74d22757be541e5f4a38d8888e
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-05-02 19:38:02 +0300

    Add a base of the BenchmarkSyncMap() benchmark

--- synchronized_hash_map_bench_test.go
+++ synchronized_hash_map_bench_test.go
@@ -11,6 +11,38 @@ const (
 	sizeForSyncBench = 1000
 )
 
+func BenchmarkSyncMap(benchmark *testing.B) {
+	for _, data := range []struct {
+		name      string
+		prepare   func() *sync.Map
+		benchmark func(syncMap *sync.Map)
+	}{
+		// TODO: add benchmark cases
+	} {
+		for threads := 1; threads <= 1e3; threads *= 10 {
+			name := fmt.Sprintf("%s/%d/%d", data.name, sizeForSyncBench, threads)
+			benchmark.Run(name, func(benchmark *testing.B) {
+				syncMap := data.prepare()
+				benchmark.ResetTimer()
+
+				for i := 0; i < benchmark.N; i++ {
+					var waiter sync.WaitGroup
+					waiter.Add(threads)
+
+					for j := 0; j < threads; j++ {
+						go func() {
+							defer waiter.Done()
+							data.benchmark(syncMap)
+						}()
+					}
+
+					waiter.Wait()
+				}
+			})
+		}
+	}
+}
+
 func BenchmarkSynchronizedHashMap(benchmark *testing.B) {
 	for _, data := range []struct {
 		name      string
