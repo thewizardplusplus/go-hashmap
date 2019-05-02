8314c54bdbcd676fce285b11c8b45431b5ba0b64
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-05-02 19:50:32 +0300

    Add a base of the BenchmarkSynchronizedBuiltinMap() benchmark

--- synchronized_hash_map_bench_test.go
+++ synchronized_hash_map_bench_test.go
@@ -7,9 +7,72 @@ import (
 	"testing"
 )
 
+type SynchronizedBuiltinMap struct {
+	innerMap map[int]int
+	lock     sync.RWMutex
+}
+
+func NewSynchronizedBuiltinMap() *SynchronizedBuiltinMap {
+	innerMap := make(map[int]int)
+	return &SynchronizedBuiltinMap{innerMap: innerMap}
+}
+
+func (builtinMap *SynchronizedBuiltinMap) Get(key int) int {
+	builtinMap.lock.RLock()
+	defer builtinMap.lock.RUnlock()
+
+	return builtinMap.innerMap[key]
+}
+
+func (builtinMap *SynchronizedBuiltinMap) Set(key int, value int) {
+	builtinMap.lock.Lock()
+	defer builtinMap.lock.Unlock()
+
+	builtinMap.innerMap[key] = value
+}
+
+func (builtinMap *SynchronizedBuiltinMap) Delete(key int) {
+	builtinMap.lock.Lock()
+	defer builtinMap.lock.Unlock()
+
+	delete(builtinMap.innerMap, key)
+}
+
 const (
 	sizeForSyncBench = 1000
 )
+
+func BenchmarkSynchronizedBuiltinMap(benchmark *testing.B) {
+	for _, data := range []struct {
+		name      string
+		prepare   func() *SynchronizedBuiltinMap
+		benchmark func(builtinMap *SynchronizedBuiltinMap)
+	}{
+		// TODO: add benchmark cases
+	} {
+		for threads := 1; threads <= 1e3; threads *= 10 {
+			name := fmt.Sprintf("%s/%d/%d", data.name, sizeForSyncBench, threads)
+			benchmark.Run(name, func(benchmark *testing.B) {
+				builtinMap := data.prepare()
+				benchmark.ResetTimer()
+
+				for i := 0; i < benchmark.N; i++ {
+					var waiter sync.WaitGroup
+					waiter.Add(threads)
+
+					for j := 0; j < threads; j++ {
+						go func() {
+							defer waiter.Done()
+							data.benchmark(builtinMap)
+						}()
+					}
+
+					waiter.Wait()
+				}
+			})
+		}
+	}
+}
 
 func BenchmarkSyncMap(benchmark *testing.B) {
 	for _, data := range []struct {
