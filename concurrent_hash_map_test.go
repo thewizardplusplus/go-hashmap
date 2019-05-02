e010cbd453e99567b1a71537ba3cd1298096ff78
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-05-02 23:45:31 +0300

    Implement of the TestConcurrentHashMap() test

--- concurrent_hash_map_test.go
+++ concurrent_hash_map_test.go
@@ -23,7 +23,104 @@ func TestConcurrentHashMap(test *testing.T) {
 		wantValue   interface{}
 		wantOk      assert.BoolAssertionFunc
 	}{
-		// TODO: add test cases
+		{
+			name:        "getting by a nonexistent key",
+			makeHashMap: func() ConcurrentHashMap { return NewConcurrentHashMap() },
+			makeKey: func() Key {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				return key
+			},
+			wantValue: nil,
+			wantOk:    assert.False,
+		},
+		{
+			name: "setting by a nonexistent key",
+			makeHashMap: func() ConcurrentHashMap {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+				// it's called inside the HashMap.Get() method below
+				key.On("Equals", mock.Anything).Return(true)
+
+				hashMap := NewConcurrentHashMap()
+				hashMap.Set(key, "five")
+
+				return hashMap
+			},
+			makeKey: func() Key {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				return key
+			},
+			wantValue: "five",
+			wantOk:    assert.True,
+		},
+		{
+			name: "setting by an existing key",
+			makeHashMap: func() ConcurrentHashMap {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+				key.On("Equals", mock.Anything).Return(true)
+
+				hashMap := NewConcurrentHashMap()
+				hashMap.Set(key, "five #1")
+				hashMap.Set(key, "five #2")
+
+				return hashMap
+			},
+			makeKey: func() Key {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				return key
+			},
+			wantValue: "five #2",
+			wantOk:    assert.True,
+		},
+		{
+			name: "deleting by a nonexistent key",
+			makeHashMap: func() ConcurrentHashMap {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				hashMap := NewConcurrentHashMap()
+				hashMap.Delete(key)
+
+				return hashMap
+			},
+			makeKey: func() Key {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				return key
+			},
+			wantValue: nil,
+			wantOk:    assert.False,
+		},
+		{
+			name: "deleting by an existing key",
+			makeHashMap: func() ConcurrentHashMap {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+				key.On("Equals", mock.Anything).Return(true)
+
+				hashMap := NewConcurrentHashMap()
+				hashMap.Set(key, "five")
+				hashMap.Delete(key)
+
+				return hashMap
+			},
+			makeKey: func() Key {
+				key := new(MockKey)
+				key.On("Hash").Return(5)
+
+				return key
+			},
+			wantValue: nil,
+			wantOk:    assert.False,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			hashMap := data.makeHashMap()
