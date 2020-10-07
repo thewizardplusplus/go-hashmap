b089752dee78a5981bbc9ae3a2a095d6957e0a54
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-07 23:00:00 +0300

    Add the hashmap.WithInnerMap() option

--- synchronized_option.go
+++ synchronized_option.go
@@ -7,3 +7,13 @@ type SynchronizedConfig struct {
 
 // SynchronizedOption ...
 type SynchronizedOption func(options *SynchronizedConfig)
+
+// WithInnerMap ...
+//
+// Default: an instance of the HashMap structure with default options.
+//
+func WithInnerMap(innerMap Storage) SynchronizedOption {
+	return func(options *SynchronizedConfig) {
+		options.innerMap = innerMap
+	}
+}
