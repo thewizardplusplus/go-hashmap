9191655605a8d0cac4cf40c70073dcbb84c512f5
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-25 23:00:00 +0300

    Add the hashmap.WithInterruption() function

--- handler.go
+++ handler.go
@@ -1,4 +1,20 @@
 package hashmap
 
+import (
+	"context"
+)
+
 // Handler ...
 type Handler func(key Key, value interface{}) bool
+
+// WithInterruption ...
+func WithInterruption(ctx context.Context, handler Handler) Handler {
+	return func(key Key, value interface{}) bool {
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+			return handler(key, value)
+		}
+	}
+}
