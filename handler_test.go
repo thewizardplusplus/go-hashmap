10cb997cba15131801a3b9ff1930a5e1b86b6b64
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-25 23:00:00 +0300

    Add a base of tests for the hashmap.WithInterruption() function

---
+++ handler_test.go
@@ -0,0 +1,47 @@
+package hashmap
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithInterruption(test *testing.T) {
+	type wrapperArgs struct {
+		ctx     context.Context
+		handler HandlerInterface
+	}
+	type handlerArgs struct {
+		key   Key
+		value interface{}
+	}
+
+	for _, data := range []struct {
+		name        string
+		wrapperArgs wrapperArgs
+		handlerArgs handlerArgs
+		want        assert.BoolAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			handler := WithInterruption(
+				data.wrapperArgs.ctx,
+				data.wrapperArgs.handler.Handle,
+			)
+			require.NotNil(test, handler)
+
+			got := handler(data.handlerArgs.key, data.handlerArgs.value)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.wrapperArgs.handler,
+				data.handlerArgs.key,
+			)
+			data.want(test, got)
+		})
+	}
+}
